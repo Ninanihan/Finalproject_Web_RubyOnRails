40ab933151564c803aeab796e3715a4b4cf924a8
Han Ni <nihan1994@163.com>
2016-03-01 13:23:52 +0000

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        remember @user
+        flash[:success] = "Welcome to the Dog World!"
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
